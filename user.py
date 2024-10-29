d126a5a45adc77a98af51bf3d38b7d1ea3ed9101
areapmaod <lyeslmr0@gmail.com>
2024-10-29 15:37:14 +0100

    add base de données avec chiffrement et hachage et dechiffrement des password_entry lorsque le mdp du coffre est valide

--- user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-        self.coffres = []
-
-    def add_coffre(self, coffre):
-        self.coffres.append(coffre)
